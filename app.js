15d8ab312a3665bda63937871f01962e6b1abb42
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-16 20:45:36 +0500

    P

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+var userInput = +prompt("Enter Percentage");
 
+if (userInput >= 80 && userInput < 101) {
+  document.write("Your Grade is A+");
+} else if (userInput >= 70 && userInput < 80) {
+  document.write("Your Grade is A");
+}
